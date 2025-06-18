41847611a263cd7ea48d912addff137b05610d00
Adminkeyan <kaishwarthi0321@gmail.com>
2025-06-18 09:33:24 +0530

    Update rds.tf

--- Terraform/rds.tf
+++ Terraform/rds.tf
@@ -1,13 +1,14 @@
+
 resource "aws_db_instance" "rds" {
-  engine         = "postgres"
-  instance_class = "db.t3.micro"
-  allocated_storage = 20
-  name           = "rorappdb"
-  username       = var.db_username
-  password       = var.db_password
-  db_subnet_group_name = aws_db_subnet_group.db_subnet.name
+  engine                 = "postgres"
+  instance_class         = "db.t3.micro"
+  allocated_storage      = 20
+  name                   = "rorappdb"
+  username               = var.db_username
+  password               = var.db_password
+  db_subnet_group_name   = aws_db_subnet_group.db_subnet.name
   vpc_security_group_ids = [aws_security_group.db.id]
-  skip_final_snapshot   = true
+  skip_final_snapshot    = true
 }
 
 resource "aws_db_subnet_group" "db_subnet" {
